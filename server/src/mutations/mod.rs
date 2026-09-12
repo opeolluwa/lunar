96362c7cb125757b81ee6719b04ee6c4ae00f718
Adefemi Adeoye <adefemiadeoye@yahoo.com>
2026-09-12 23:58:11 +0100

    remove dead code

--- server/src/mutations/mod.rs
+++ server/src/mutations/mod.rs
@@ -1,10 +1,1 @@
-pub mod bookmark;
-pub mod notes;
 pub mod preflight;
-pub mod recycle_bin;
-pub mod reminder;
-pub mod snippets;
-pub mod sync_queue;
-pub mod todo;
-pub mod workspace;
-pub mod workspace_preference;
